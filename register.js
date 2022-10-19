fd6f787d16f8b4ac5c2daa8071297c2e8687b5ca
LauricellaAndrea <andrealauricella97@gmail.com>
2022-10-19 21:41:05 +0200

    estrazione file

---
+++ register.js
@@ -0,0 +1,4 @@
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const name = document.querySelector("#name");
+const age = document.querySelector("#age"); 
